e6bfc387e3708065d77b78b63018e1661893eb6a
Capucine Trossat <ctrossat.dev@gmail.com>
2025-03-13 22:00:10 +0100

    made chat interface better

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   const [isConnected, setIsConnected] = useState(false);
   const [transport, setTransport] = useState("N/A");
   const [lastMessage, setLastMessage] = useState("HELLO WORLD");
+  const [messages, setMessages] = useState<Array<String>>([]);
 //   const [messages, setMessages] = useState<Array<String>>([]);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -35,6 +36,7 @@ export default function Home() {
     socket.on("disconnect", onDisconnect);
     socket.on("message", (message) => {
       setLastMessage(message);
+      setMessages((prev) => [message, ...prev]);
     });
 
     return () => {
@@ -46,33 +48,41 @@ export default function Home() {
 
   const onClick = () => {
     const inputValue = inputRef.current?.value;
-    inputRef.current!.value = "";
-    console.log("Emitting event F with data:", inputValue);
-    socket.emit("message", inputValue);
+    if (inputValue != "") {
+      inputRef.current!.value = "";
+      console.log("Emitting event F with data:", inputValue);
+      socket.emit("message", inputValue);
+    }
   };
 
+  console.log(messages);
+
   return (
-    <div className="h-screen flex flex-col items-center justify-aroundmy-4">
-        <div>
+    <div className="h-screen flex flex-col items-center">
+        <div className="absolute left-0">
             <p>Status: {isConnected ? "connected" : "disconnected"}</p>
             <p>Transport: {transport}</p>
         </div>
-        <div className="flex flex-1 flex-col-reverse w-[50rem] mb-8 bg-black p-4 text-3xl font-omori border-4 border-white w-[40rem]">
+        <div className="flex flex-col w-[50rem] my-8">
             <input 
             type="text" 
             ref={inputRef} 
-            className="bg-white text-black" 
+            className="bg-white text-black p-2 mb-4" 
             onKeyDown={(e) => {
                 if (e.key === 'Enter') {
                 onClick();
                 }
             }}
             />
-            <button onClick={onClick}>Send</button>
-            <div className="felx flex-col items-center">
-                <p className="text-center mb-8 font-omori text-3xl">{`Last message sent: ${lastMessage}`}</p>
-            </div>
+            <button className='bg-black py-1 px-4 text-3xl font-omori border-4 border-white w-fit' onClick={onClick}>Send</button>
         </div>
+          <div className="flex flex-1 flex-col items-center">
+            <h2 className="font-omori text-3xl mb-5">Message history:</h2>
+            {messages.map((message, index) => (
+                <p key={index} className="text-left font-omori text-2xl w-full">{`> ${message}`}</p>
+            ))}
+
+          </div>
     </div>
   );
 }
